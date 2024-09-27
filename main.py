8dbd85a95fdb91136999cc54f057c16a51f91758
vinicius.amancio4257@gmail.com <vinicius.amancio4257@gmail.com>
2024-09-26 21:27:59 -0300

    sincronização

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
-""" from flask import Flask, request, render_template
-from usuario import Usuario
-
-
-app = Flask(__name__)
-
-
-@app.route("/listarAlunos", methods = ['GET', 'POST'])
-def listarAlunos():
-    if request.method == 'GET':
-        return render_template("listarAlunos.html")
-    else: """
-
 from flask import Flask, render_template, redirect, url_for, request, session, flash, jsonify
-from conexao import Conexao
 from usuario import Usuario
-
-
 
 app = Flask(__name__)
 app.secret_key = "banana"
 
 @app.route("/")
 def pag_inicio():
-    return render_template('index.html')
-
+    return render_template('pag-inicial.html')
 
 @app.route("/cadastrar-motorista", methods=['GET','POST'])
-def pag_cadastro():
+def pag_cadastro_motorista():
     if request.method == 'GET':
-        return render_template('pag-motorista.html')
+        return render_template('cadastro-motorista.html')
     else:
         nome = request.form["nome"]
         cpf = request.form["cpf"]
@@ -38,102 +21,81 @@ def pag_cadastro():
         email = request.form["email"]
         senha = request.form["senha"]
         cidade = request.form["cidade"]
-        endereco = request.form["endereco"]
+        endereco = request.form["endereço"]
+        foto_motorista = request.form["ft_motorista"]
+        foto_van = request.form["ft_veiculo"]
+        valor_mensalidade = request.form["valor_cobrado"]
 
         usuario = Usuario()
-        if usuario.cadastrar(nome, cpf, cnpj, cnh, telefone, email, senha, cidade, endereco):
-            return 'CADASTRO COM SUCESSO'
+        if usuario.cadastrar_motorista(nome, cpf, cnh, cnpj, telefone, email, senha, cidade, endereco, foto_motorista, foto_van, valor_mensalidade):
+            return render_template('login-motorista.html') 
+        
         else:
-            return 'ERRO AO CADASTRAR'
+            return render_template('cadastro-motorista.html')
+        
+        
 
 @app.route("/cadastrar-aluno", methods=['GET','POST'])
-def pag_cdaluno():
+def pag_cadastro_aluno():
     if request.method == 'GET':
-        return render_template('pag-aluno.html')
-    else: 
-        
-        nome_aluno = request.form["nome_aluno"]
-        endereco = request.form["endereco"]
-        cidade = request.form["cidade"]
-        idade = request.form["idade"]
-        nome_responsavel = request.form["nome_responsavel"]
-        tel_responsavel = request.form["tel_responsavel"]
+        return render_template('cadastro-aluno.html')
+    else:
+        nome_aluno = request.form["nomeAluno"]
+        escola = request.form["escola"]
+        foto_aluno = request.form["ft_aluno"]
+        condicao_medica = request.form["condicao_medica"]
+        nome_responsavel = request.form["nomeResponsavel"]
+        cpf_responsavel = request.form["cpfResponsavel"]
+        endereco_responsavel = request.form["endereço"]
+        tel_responsavel = request.form["telefoneResponsavel"]
+        email_responsavel = request.form["emailAluno"]
+        senha_responsavel = request.form["senhaAluno"]
 
         usuario = Usuario()
-        if usuario.cadastrar_aluno(nome_aluno, endereco, cidade, idade, nome_responsavel, tel_responsavel):
-            return 'ALUNO CADASTRADO COM SUCESSO'
+        if usuario.cadastrar_aluno(nome_aluno, foto_aluno, condicao_medica, escola, nome_responsavel, cpf_responsavel, endereco_responsavel, tel_responsavel, email_responsavel, senha_responsavel):
+             return render_template('login-aluno.html') 
         else:
-            return 'ERRO AO CADASTRAR'
+           return render_template('cadastro-aluno.html')
+        
+@app.route("/logar_get_aluno", methods=['GET'])
+def logar_get_aluno():
+    return render_template("login-aluno.html")
 
-@app.route("/login-aluno", methods=['GET','POST'])
-def login_aluno():
-    # Obtém os dados enviados pelo formulário de login (ID e nome do aluno)
-    id_aluno = request.form["id_aluno"]
-    nome_aluno = request.form["nome_aluno"]
+@app.route("/logar_get_motorista", methods=['GET'])
+def logar_get_motorista():
+    return render_template("login-motorista.html")
 
-    # Estabelece a conexão com o banco de dados
-    mydb = Conexao.conectar()
-    mycursor = mydb.cursor()
-
-    # Define a consulta SQL para buscar um aluno específico usando ID e nome
-    sql = "SELECT id_aluno FROM tb_alunos WHERE id_aluno = %s AND nome_aluno = %s"
-    # Executa a consulta SQL com os parâmetros fornecidos
-    mycursor.execute(sql, (id_aluno, nome_aluno))
-    # Obtém o resultado da consulta (uma linha ou None)
-    resultado = mycursor.fetchone()
-
-    # Verifica se o resultado contém dados (ou seja, se as credenciais estão corretas)
-    if resultado:
-        # Se as credenciais forem válidas, armazena o ID do aluno na sessão
-        session['id_aluno'] = id_aluno
-        # Redireciona para a página principal do aluno
-        return redirect(url_for('pagina_aluno'))
+@app.route("/logar", methods=['POST', 'GET'])
+def logar():
+    if request.method == 'GET':
+        return render_template("login-motorista.html")
     else:
-        # Se as credenciais forem inválidas, exibe uma mensagem de erro e reexibe o formulário de login
-        flash("Credenciais inválidas")
-        return render_template('login_aluno.html')
-
-@app.route("/pagina-aluno")
-def pagina_aluno():
-    # Verifica se o ID do aluno está armazenado na sessão (se o aluno está autenticado)
-    if 'id_aluno' not in session:
-        # Se o ID do aluno não estiver na sessão, redireciona para a página de login
-        return redirect(url_for('login_aluno'))
-    # Se o aluno estiver autenticado, exibe uma mensagem de boas-vindas com o ID do aluno
-    return "Bem-vindo à página do aluno, ID: " + session['id_aluno']
-
-@app.route("/login-motorista", methods=["POST"])
-def login_motorista():
-    email = request.form["email"]
-    senha = request.form["senha"]
-
-    mydb = Conexao.conectar()
-    mycursor = mydb.cursor()
-
-    # Buscar o motorista pelo e-mail
-    sql = "SELECT cpf, senha FROM tb_motoristas WHERE email = %s"
-    mycursor.execute(sql, (email,))
-    resultado = mycursor.fetchone()
-
-    if resultado:
-        cpf, senha_hash = resultado
-        # Verificar a senha fornecida com o hash armazenado
-        if bycrypt.checkpw(senha.encode('utf-8'), senha_hash.encode('utf-8')):
-            session['cpf_motorista'] = cpf
-            return redirect(url_for('listar-aluno.html'))
+        senha = request.form['senha']
+        email = request.form['email']
+        usuario = Usuario()
+        if usuario.logar(email, senha):
+            session['usuario_logado'] = {
+                "nome": usuario.nome,
+                "email": usuario.email,
+                "cpf": usuario.cpf
+            }
+            return render_template("pag-inicial-motorista.html")
         else:
-            flash("Credenciais inválidas")
-            return render_template('login_motorista.html')
-    else:
-        flash("Credenciais inválidas")
-        return render_template('login_motorista.html')
+            session.clear()
+            return redirect("/logar")
 
-@app.route("/pagina-motorista")
-def pagina_motorista():
-    if 'cpf_motorista' not in session:
-        return redirect(url_for('login_motorista'))
-    return "Bem-vindo à página do motorista, CPF: " + session['cpf_motorista']
+@app.route("/listar-motorista", methods=['GET', 'POST'])
+def listar_motorista():
+    if request.method == 'GET':
+        usuario = Usuario()
+        lista_usuarios = usuario.listar_usuario()
+        return render_template("listar-motorista.html", usuarios=lista_usuarios)
+
+@app.route("/listar-alunos", methods=['GET', 'POST'])
+def listar_alunos():
+    if request.method == 'GET':
+        usuario = Usuario()
+        lista_alunos = usuario.listar_aluno()
+        return render_template("listar-aluno.html", alunos=lista_alunos)
 
 app.run(debug=True)
-
-    
