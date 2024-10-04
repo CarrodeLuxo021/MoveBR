e78b6821d23e675251b353d954094c88f858403f
André Bido <andre.bido4406@gmail.com>
2024-10-04 14:52:10 -0300

    .

--- pagamentos.py
+++ pagamentos.py
@@ -12,8 +12,19 @@ class Pagamentos():
             mydb = Conexao.conectar()
             mycursor = mydb.cursor()
 
-            sql = f"""INSERT INTO historico_pagamentos (id_aluno, data_pagamento, mes_pagamento, valor_pagamento)
-            VALUES ('{id_aluno}', '{mes}', '{data}', '{valor}');
+            # Primeiro, você faz um JOIN para buscar o nome do aluno na tabela tb_alunos
+            sql_busca_nome = f"""
+            SELECT nome_aluno 
+            FROM tb_alunos 
+            WHERE id_aluno = {id_aluno};
+            """
+            mycursor.execute(sql_busca_nome)
+            nome_aluno = mycursor.fetchone()[0]  # Captura o nome do aluno
+
+            # Agora, você insere na tabela historico_pagamentos com o nome do aluno
+            sql = f"""
+            INSERT INTO historico_pagamentos (id_aluno, nome_aluno, data_pagamento, mes_pagamento, valor_pagamento)
+            VALUES ('{id_aluno}', '{nome_aluno}', '{data}', '{mes}', '{valor}');
             """
             mycursor.execute(sql)
 
@@ -21,10 +32,9 @@ class Pagamentos():
             mycursor.close()
 
             return True
-        
-        except Exception as e:
 
-            print(f"Erro ao cadastrar aluno: {e}")
+        except Exception as e:
+            print(f"Erro ao cadastrar pagamento: {e}")
             return False
         
     def listar_historico(self, mes, cpf_motorista):
