51a3450d9274da4c95514597144924fe64a3846e
Vinicius Amancio <vinicius.amancio4257@gmail.com>
2024-11-01 17:20:16 -0300

    fundo movel

--- static/js/transicao-pagina.js
+++ static/js/transicao-pagina.js
@@ -1,20 +1,15 @@
 document.getElementById('transitionButton').addEventListener('click', function() {
-    const transitionDiv = document.getElementById('transition');
-    const dustDiv = document.getElementById('dust');
+    const vehicle = document.querySelector('.vehicle');
+    const dust = document.querySelector('.dust');
 
     // Iniciar a animação do veículo
-    const vehicle = document.querySelector('.vehicle');
-    vehicle.style.animation = 'moveVehicle 1s forwards';
-    
-    
-    
-    setTimeout(() => {
-        transitionDiv.style.display = 'flex'; // Mostrar a animação de transição
-        dustDiv.style.animation = 'dustEffect 1s forwards'; // Iniciar a animação de poeira
-    }, 2000);
+    vehicle.style.animation = 'moveVehicle 4s linear forwards';
 
-    // Esperar a animação terminar antes de mudar de página
+    // Iniciar a animação da poeira
+    dust.style.animation = 'moveDust 10s linear forwards';
+
+    // Redirecionar após a conclusão da animação do veículo
     setTimeout(() => {
-        window.location.href = 'pag-inicial-motorista.html'; // Substitua pelo URL da próxima página
-    }, 1000); // O tempo deve ser igual à duração da animação
+        window.location.href = 'pag-inicial-motorista.html'; // Altere para sua próxima página
+    }, 4000); // Tempo sincronizado com a animação
 });
